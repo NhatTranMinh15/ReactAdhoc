c6378c6eb44bb1795153bf1176fb504998988e72
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-12-23 15:25:23 +0700

    Create React App

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { RouterProvider, } from "react-router";
+import appRouter from "./app.router";
+import { AuthenticatedProvider } from "./shared/AuthenticatedContext";
+
+function App() {
+    return (
+        <AuthenticatedProvider>
+            <RouterProvider router={appRouter} />
+        </AuthenticatedProvider>
+    )
+}
+
+export default App;
---
+++ src/app.router.tsx
@@ -0,0 +1,15 @@
+import authRoutes from "./pages/auth/auth.routes";
+import pagesRoutes from "./pages/pages.routes";
+import {createBrowserRouter, Navigate} from "react-router-dom";
+
+const appRouter = createBrowserRouter([
+    {
+        path: '',
+        element: <Navigate to="/pages" replace />
+    },
+    ...pagesRoutes,
+    ...authRoutes,
+])
+
+
+export default appRouter
